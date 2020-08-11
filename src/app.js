9f1340230563d483aa1ad1953a5ec51738de4660
leandrofahur <leandrofahur@gmail.com>
2020-08-10 23:26:55 -0700

    calls the forecast function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Sanity check...');
+// Core modules:
+const path = require('path');
+
+// Third-party modules:
+const request = require('request');
+const { urlencoded } = require('express');
+
+// Utils:
+const forecast = require('../utils/forecast');
+
+forecast('Surrey', 'British Columbia', (err, res) => {
+  if(err) {
+    return console.log(err);
+  }
+  console.log(res);
+})
