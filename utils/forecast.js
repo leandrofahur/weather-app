cf25e12ec48d4151aa344f5cc2cacfffdf5c6c88
leandrofahur <leandrofahur@gmail.com>
2020-08-10 23:26:28 -0700

    forecast function makes the request to the weatherstack api

---
+++ utils/forecast.js
@@ -0,0 +1,36 @@
+// Third-party modules:
+const request = require('request');
+const { urlencoded } = require('express');
+
+const ACCESS_KEY = 'f0e16ba1c14b8281bc3f39a21b50d172';
+
+const forecast = (city, country, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=' + encodeURIComponent(ACCESS_KEY) + '&query=' + encodeURIComponent(city) + ' ' + encodeURIComponent(country);
+
+  request({url}, (error, response) => {
+    const data = JSON.parse(response.body);
+
+    if(data.success === false) {
+      // return console.log(data.error.info);
+      callback(data.error.info, undefined);
+    }
+    
+    const temperature = data.current.temperature;
+    const weatherDescriptions = data.current.weather_descriptions;
+    const precip = data.current.precip;
+    const weatherIcon = data.current.weather_icons;
+    const fellsLike = data.current.feelslike;
+
+    const forecastData = {
+      temperature,
+      fellsLike,
+      precip,
+      weatherIcon,
+      weatherDescriptions
+    }
+    // console.log(forecastData);
+    callback(undefined, forecastData);
+  });
+};
+
+module.exports = forecast;
